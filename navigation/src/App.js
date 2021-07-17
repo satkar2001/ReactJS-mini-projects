432d71257157019086ed7487d2b012441f5d2446
satkar2001 <satkaracharya883@gmail.com>
2021-07-17 11:59:52 +0530

    .

--- navigation/src/App.js
+++ navigation/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar';
 function App() {
   return (
     <div className="App">
-      <h1>react navbar</h1>
+      <Navbar/>
     </div>
   );
 }
