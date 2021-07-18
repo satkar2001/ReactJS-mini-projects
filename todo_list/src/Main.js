9c5cc0e3f4732f458ddcc0b6329568008136c60f
satkar2001 <satkaracharya883@gmail.com>
2021-07-18 19:14:53 +0530

    duparray added

--- todo_list/src/Main.js
+++ todo_list/src/Main.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React,{useState}from 'react'
 
 export default function Main() {
+    const[taskname,settaskname]= useState('')
+    const[tasklist,settasklist]=useState([])
+    function addtask(){
+       settasklist([...tasklist,taskname])
+    }
+    function deletetask(index){
+      var duparray=[...tasklist]
+      duparray.splice(index,1)//splice is a js function to add or delete indexx of array
+      settasklist(duparray)
+    }
+    const tasklistcontent=tasklist.map((task,index)=>{ //in map functions index no is provided just by writing index
+        return(
+            <div>
+                <p>{task}</p>
+                <i className="far fa-trash-alt" onClick={()=>deletetask(index)}></i>
+            </div>
+        )
+    })
+
     return (
         <div>
             <div className="row justify-content-center">
-                <div className="col-md-5">
+                <div className="col-md-5 shadow p-3 mb-5 bg-white rounded">
                     <h1>TODO LIST</h1>
-                    <input type="text" placeholder="enter task" className='form-control'/>
-                    <button className="btn btn-success">add tas</button>
+                    <input type="text" placeholder="enter task" className='form-control' value={taskname}
+                    onChange={(e)=>{settaskname(e.target.value)}}/>
+                    <button className="btn btn-primary" onClick={addtask}>add task</button>
+                    <br />
+                    {tasklistcontent}
                 </div>
             </div>
         </div>
