3f4205fff15eb12a654f7009bf6802ea2f376ad3
satkar2001 <satkaracharya883@gmail.com>
2021-07-24 14:07:59 +0530

    .

---
+++ seafood_react/src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer(){
+
+    return(
+        <div style={{backgroundImage:'linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%)'}}>
+            <p >designed and developed by :
+            </p>
+            <p >Satkar Acharya</p>
+        </div>
+    )
+
+}
+export default Footer
---
+++ seafood_react/src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+function Navbar(){
+    return(
+        <div style={{backgroundImage:'linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%)'}}>
+            <h1>seafoods</h1>
+            <a style={{margin:'10px',color:'white'}}>Home</a>
+            <a style={{margin:'10px',color:'white'}}>Order</a>
+            <a style={{margin:'10px',color:'white'}}>Contact Us</a>
+        </div>
+    )
+}
+export default Navbar
